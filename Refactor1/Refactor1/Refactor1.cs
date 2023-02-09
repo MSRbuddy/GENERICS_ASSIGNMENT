81e129c9550eb3412c82e0398f1a5a4231879507
MSRbuddy <meghanadasari222@gmail.com>
2023-02-09 22:46:39 +0530

    Added: Refactor-1

---
+++ Refactor1/Refactor1/Refactor1.cs
@@ -0,0 +1,46 @@
+﻿namespace Refactor1
+{
+    internal class Refactor1
+    {
+        public class GenericMax<T> where T : IComparable
+        {
+            public T firstvalue, secondvalue, thirdvalue;
+            public GenericMax(T firstvalue, T secondvalue, T thirdvalue)
+            {
+                this.firstvalue = firstvalue;
+                this.secondvalue = secondvalue;
+                this.thirdvalue = thirdvalue;
+            }
+            public GenericMax()
+            { 
+            }
+            public static T MaxValue(T firstvalue, T secondvalue, T thirdvalue)
+            {
+                if (firstvalue.CompareTo(secondvalue) > 0 && firstvalue.CompareTo(thirdvalue) > 0)
+                {
+                    return firstvalue;
+                }
+                if (secondvalue.CompareTo(firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) > 0)
+                {
+                    return secondvalue;
+                }
+                if (thirdvalue.CompareTo(firstvalue) > 0 && thirdvalue.CompareTo(secondvalue) > 0)
+                {
+                    return thirdvalue;
+                }
+                return default;
+            }
+            public T MaxMethod()
+            {
+                T max = GenericMax<T>.MaxValue(this.firstvalue, this.secondvalue, this.thirdvalue);
+                return max;
+
+            }
+            static void Main()
+            {
+                GenericMax<int> generic = new GenericMax<int>(5, 3, 1);               
+                generic.MaxMethod();                
+            }
+        }
+    }
+}
